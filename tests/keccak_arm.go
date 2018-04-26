49073a189ea7ae14771fa4e126f2f8881306e021
Ian Johnson <ian.johnson@canonical.com>
2018-04-26 15:59:48 -0500

    Update test file (incorrect size for constants)

--- tests/keccak_arm.go
+++ tests/keccak_arm.go
@@ -2,4 +2,4 @@ package keccak
 
 // go:noescape
 // This function is implemented in keccak.s
-func KeccakF1600(state *[25]uint64, constants *[25]uint64)
+func KeccakF1600(state *[25]uint64, constants *[24]uint64)
