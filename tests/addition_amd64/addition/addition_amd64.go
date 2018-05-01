bcda7cbac8f2bf6c7fd1b1e9eaab4eb76cec126d
Ian Johnson <ian.johnson@canonical.com>
2018-05-01 10:06:12 -0500

    tests: add docstring comments on exported assembly functions

--- tests/addition_amd64/addition/addition_amd64.go
+++ tests/addition_amd64/addition/addition_amd64.go
@@ -1,5 +1,6 @@
 package addition
 
 //go:noescape
+// Add2 returns the sum of two numbers
 // This function is implemented in addition.s
 func Add2(x, y int) int
--- tests/keccak_arm/keccak/keccak_arm.go
+++ tests/keccak_arm/keccak/keccak_arm.go
@@ -1,5 +1,6 @@
 package keccak
 
 // go:noescape
+// KeccakF1600 permutes the state using the provided permutation constants
 // This function is implemented in keccak.s
 func KeccakF1600(state *[25]uint64, constants *[24]uint64)
